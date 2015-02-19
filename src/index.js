e2abfc6f2379184a85c0bec56f9666f717636148
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-19 12:39:48 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -40,33 +40,6 @@ crypto.endian = function(n) {
     }
 };
 
-crypto.wordsToBytes = function(words) {
-    var bytes = [],
-        i = 0,
-        il = words.length * 32;
-
-    while (i < il) {
-        bytes.push((words[i >>> 5] >>> (24 - i % 32)) & 0xFF);
-        i += 8;
-    }
-
-    return bytes;
-};
-
-crypto.bytesToWords = function(bytes) {
-    var words = [],
-        i = -1,
-        il = bytes.length - 1,
-        b = 0;
-
-    while (i++ < il) {
-        words[b >>> 5] |= bytes[i] << (24 - b % 32);
-        b += 8;
-    }
-
-    return words;
-};
-
 if (globalCrypto && isFunction(globalCrypto.getRandomValues)) {
     randomBytes = function(size) {
         return crypto.getRandomValues(new NativeUint8Array(size));
